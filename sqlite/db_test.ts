aa52c2306254a14ebc0ebd42498c73a12368ad50
Ethan Braunstein <braunstein.ethan@gmail.com>
2025-02-27 12:24:25 -0500

    Changed sqlite testing

--- sqlite/db_test.ts
+++ sqlite/db_test.ts
@@ -4,7 +4,6 @@ import { Expansion } from "./types.ts";
 
 // db must be migrated/seeded
 Deno.test("db test 1", () => {
-    console.log(Deno.env.get("DB_PATH"));
     const rows: Expansion[] = <Expansion[]>db.prepare("SELECT * FROM expansion").all();
     db.close();
     console.log(rows);
