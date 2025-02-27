119f0ec0716106c533524d6ec305c446d8b59669
Ethan Braunstein <braunstein.ethan@gmail.com>
2025-02-27 18:15:39 -0500

    Added basic auth example

--- islands/LoginBox.tsx
+++ islands/LoginBox.tsx
@@ -1,10 +1,9 @@
-import { Handlers } from "$fresh/server.ts";
-
 export default function LoginBox() {
     return (
         <div class="login-box">
             <h3 class="title">Login</h3>
-            <form class="login-form">
+            {/* Sends a POST request to /api/login endpoint */}
+            <form class="login-form" method="post" action="/api/login">
                 <input class="text-input" type="text" name="username" placeholder="Username" />
                 <input class="text-input" type="password" name="password" placeholder="Password" />
                 <button class="submit-button" type="submit">Login</button>
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,11 +1,25 @@
 import LoginBox from "../islands/LoginBox.tsx";
+import type { Handlers, PageProps } from "$fresh/server.ts";
+import { getCookies } from "@std/http/cookie";
 
-export default function Home() {
+interface Data {
+    isAllowed: boolean;
+}
+
+export const handler: Handlers<Data> = {
+    GET(req, ctx) {
+        const cookies = getCookies(req.headers);
+        return ctx.render!({ isAllowed: cookies.auth === "uniqueValue" });
+    },
+};
+
+export default function Home({ data }: PageProps<Data>) {
     return (
         <div>
             <h1>PTCGP Trader</h1>
             <div class="content">
-                <LoginBox />
+                You currently {data.isAllowed ? "are" : "are not"} logged in.
+                {data.isAllowed ? <a href="/api/logout">Logout</a> : <LoginBox />}
             </div>
         </div>
     );
