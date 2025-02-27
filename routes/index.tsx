2f3cd03e0e98e425740db1f509df9e189939de4f
Ethan Braunstein <braunstein.ethan@gmail.com>
2025-02-27 18:40:14 -0500

    Renamed var

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,13 +3,13 @@ import type { Handlers, PageProps } from "$fresh/server.ts";
 import { getCookies } from "@std/http/cookie";
 
 interface Data {
-    isAllowed: boolean;
+    isLoggedIn: boolean;
 }
 
 export const handler: Handlers<Data> = {
     GET(req, ctx) {
         const cookies = getCookies(req.headers);
-        return ctx.render!({ isAllowed: cookies.auth === "uniqueValue" });
+        return ctx.render!({ isLoggedIn: cookies.auth === "uniqueValue" });
     },
 };
 
@@ -18,8 +18,8 @@ export default function Home({ data }: PageProps<Data>) {
         <div>
             <h1>PTCGP Trader</h1>
             <div class="content">
-                You currently {data.isAllowed ? "are" : "are not"} logged in.
-                {data.isAllowed ? <a href="/api/logout">Logout</a> : <LoginBox />}
+                You currently {data.isLoggedIn ? "are" : "are not"} logged in.
+                {data.isLoggedIn ? <a href="/api/logout">Logout</a> : <LoginBox />}
             </div>
         </div>
     );
